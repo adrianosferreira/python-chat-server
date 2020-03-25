d55c4868154cc87c0c39a881cbcc253c2050b04d
Adriano Ferreira <adriano.f@onthegosystems.com>
2020-03-25 15:54:53 -0300

    fixup

---
+++ server.py
@@ -0,0 +1,36 @@
+import asyncio
+import websockets
+import json
+
+USERS = set()
+
+
+async def register(websocket):
+    USERS.add(websocket)
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+
+
+async def notify_users(msg):
+    if USERS:
+        await asyncio.wait([ws.send(json.dumps(msg)) for ws in USERS])
+
+
+async def chat(websocket, path):
+    await register(websocket)
+    try:
+        async for msg in websocket:
+            data = json.loads(msg)
+            print(data)
+            await notify_users(data)
+
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(chat, "localhost", 8881)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
